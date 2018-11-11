5580360710415ba0bacc697d5eba7dd853fdc54a
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-11 13:35:39 -0300

    Added subastify client instance to app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,35 @@
+import Axios from 'axios';
 import 'node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
+import { connect } from 'react-redux';
 
+import { ISubastifyClient, SubastifyClient } from 'src/clients';
+import { setSubastifyClient } from 'src/store/actions/clients';
 import SubastifyNavBar from './SubastifyNavBar';
 import SubastifyPageBody from './SubastifyPageBody';
 
 
-export default function App(props: any) {
-    return (
-        <React.Fragment>
-            <SubastifyNavBar />
-            <SubastifyPageBody />
-        </React.Fragment>
-    );
+interface IAppProps extends React.Props<any> {
+    setSubastifyClient: (client: ISubastifyClient) => void;
 }
+
+class App extends React.Component<IAppProps> {
+
+    public componentWillMount() {
+        this.props.setSubastifyClient(
+            new SubastifyClient(Axios.create({ baseURL: 'http://localhost:3001' }))
+        )
+    }
+
+    public render() {
+        return (
+            <React.Fragment>
+                <SubastifyNavBar />
+                <SubastifyPageBody />
+            </React.Fragment>
+        );
+    }
+}
+
+export default connect(null, { setSubastifyClient })(App);
