4c690b5fcac458bb4b9f4caf0d6fb232b3f9365b
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-11 10:52:24 -0300

    Added bootstrap & font awesome

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,10 @@
+import 'node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
 
 import SubastifyNavBar from './SubastifyNavBar';
 import SubastifyPageBody from './SubastifyPageBody';
+
 
 export default function App(props: any) {
     return (
