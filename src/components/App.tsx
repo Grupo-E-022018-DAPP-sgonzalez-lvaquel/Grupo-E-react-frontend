14cebbfa3a90e2e79a52bfadd4b97aeba05f7a3f
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-13 19:08:55 -0300

    Refactor: extracted client instanciation out of app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ISubastifyClient, SubastifyClient } from 'src/clients';
+import { ISubastifyClient } from 'src/clients';
 import { setSubastifyClient } from 'src/store/actions/clients';
 import ErrorsNotifier from './ErrorsNotifier';
 import SubastifyNavBar from './SubastifyNavBar';
@@ -11,6 +11,7 @@ import SubastifyPageBody from './SubastifyPageBody';
 
 
 interface IAppProps extends React.Props<any> {
+    subastifyClient: ISubastifyClient;
     setSubastifyClient: (client: ISubastifyClient) => void;
 }
 
@@ -18,7 +19,7 @@ class App extends React.Component<IAppProps> {
 
     public componentWillMount() {
         this.props.setSubastifyClient(
-            new SubastifyClient(Axios.create({ baseURL: 'http://localhost:3001' }))
+            this.props.subastifyClient
         )
     }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import Axios from 'axios';
 import 'node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
@@ -6,13 +7,15 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
+import { SubastifyClient } from './clients';
 import store from './store';
 
 import App from './components/App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App subastifyClient={
+            new SubastifyClient(Axios.create({ baseURL: 'http://localhost:3001' }))}/>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
