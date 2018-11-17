51747a45358a34383a85da330713578478d8d050
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-17 11:07:15 -0300

    Added auth client to store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { ISubastifyClient } from 'src/clients';
-import { setSubastifyClient } from 'src/store/actions/clients';
+import Auth from 'src/clients/Auth';
+import { setAuthService, setSubastifyClient } from 'src/store/actions';
+import AuthCallback from './AuthCallback';
 import ErrorsNotifier from './ErrorsNotifier';
 import SubastifyNavBar from './SubastifyNavBar';
 import SubastifyPageBody from './SubastifyPageBody';
 
 
 interface IAppProps extends React.Props<any> {
+    auth: Auth;
     subastifyClient: ISubastifyClient;
     setSubastifyClient: (client: ISubastifyClient) => void;
+    setAuthService: (auth: Auth) => void;
 }
 
 class App extends React.Component<IAppProps> {
 
     public componentWillMount() {
+        this.props.setAuthService(
+            this.props.auth
+        );
+
         this.props.setSubastifyClient(
             this.props.subastifyClient
         )
@@ -26,6 +34,7 @@ class App extends React.Component<IAppProps> {
         return (
             <Router>
                 <React.Fragment>
+                    <Route path="/auth" component={AuthCallback} />
                     <SubastifyNavBar />
                     <ErrorsNotifier />
                     <SubastifyPageBody />
@@ -35,4 +44,4 @@ class App extends React.Component<IAppProps> {
     }
 }
 
-export default connect(null, { setSubastifyClient })(App);
+export default connect(null, { setSubastifyClient, setAuthService })(App);
