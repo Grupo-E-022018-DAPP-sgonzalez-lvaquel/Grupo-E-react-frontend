49ba4e99bc972a7fad86b6aea71f6ded526f92e8
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-20 18:25:09 -0300

    Added auth selectors

--- src/store/selectors/auth.ts
+++ src/store/selectors/auth.ts
@@ -3,3 +3,19 @@ import { IStore } from '../reducers/rootReducer';
 export function getAuthService(state: IStore) {
     return state.auth;
 }
+
+export function getCurrentUserId(state: IStore): string | undefined {
+    const auth = getAuthService(state);
+    if (!auth) {
+        return undefined;
+    }
+    return auth.userId;
+}
+
+export function getAccessToken(state: IStore): string | undefined {
+    const auth = getAuthService(state);
+    if(!auth) {
+        return undefined;
+    }
+    return auth.accessToken;
+}
