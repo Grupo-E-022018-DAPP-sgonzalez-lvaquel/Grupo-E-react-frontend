3520410278f55ca56881752f22680261d9eec0db
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-20 18:21:10 -0300

    Added get user and get accesstoken to auth client

--- src/clients/Auth.ts
+++ src/clients/Auth.ts
@@ -1,6 +1,7 @@
 import { Auth0DecodedHash, WebAuth } from 'auth0-js';
 import { authConfig } from 'src/config';
 
+
 export class Auth {
     public auth0 = new WebAuth({
         clientID: authConfig.clientId as string,
@@ -10,6 +11,10 @@ export class Auth {
         scope: 'openid'
     });
 
+    private readonly ID_TOKEN = 'id_token';
+    private readonly ACCESS_TOKEN = 'access_token';
+    private readonly EXPIRES_AT = 'expires_at';
+    
     constructor() {
         this.login = this.login.bind(this);
         this.logout = this.logout.bind(this);
@@ -33,20 +38,42 @@ export class Auth {
 
     public setSession(authResult: Auth0DecodedHash) {
         const expiresAt = JSON.stringify(((authResult.expiresIn as number) * 1000) + new Date().getTime());
-        sessionStorage.setItem('access_token', authResult.accessToken as string);
-        sessionStorage.setItem('id_token', authResult.idToken as string);
-        sessionStorage.setItem('expires_at', expiresAt);
+        this.storage.setItem(this.ACCESS_TOKEN, authResult.accessToken as string);
+        this.storage.setItem(this.ID_TOKEN, authResult.idToken as string);
+        this.storage.setItem(this.EXPIRES_AT, expiresAt);
     }
 
     public logout() {
-        sessionStorage.removeItem('access_token');
-        sessionStorage.removeItem('id_token');
-        sessionStorage.removeItem('expires_at');
+        this.storage.removeItem(this.ACCESS_TOKEN);
+        this.storage.removeItem(this.ID_TOKEN);
+        this.storage.removeItem(this.EXPIRES_AT);
     }
 
     public isAuthenticated() {
-        const expiresAt = JSON.parse(sessionStorage.getItem('expires_at') as string);
+        const expiresAt = JSON.parse(this.storage.getItem(this.EXPIRES_AT) as string);
         return new Date().getTime() < expiresAt;
+    }
+
+    get userId(): string | undefined {
+        if(this.isAuthenticated()){
+            const id = this.storage.getItem(this.ID_TOKEN)
+            if(!id) {return undefined;}
+            return id;
+        }
+        return undefined;
+    }
+
+    get accessToken(): string | undefined {
+        if(this.isAuthenticated()){
+            const accessToken = this.storage.getItem(this.ACCESS_TOKEN)
+            if(!accessToken) {return undefined;}
+            return accessToken;
+        }
+        return undefined;
+    }
+
+    private get storage() {
+        return sessionStorage;
     }
 }
 
