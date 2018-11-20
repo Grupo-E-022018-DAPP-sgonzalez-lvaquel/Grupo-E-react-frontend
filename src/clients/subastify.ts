37601612de31849b1325a7e532fee592dee0ab5a
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-20 18:21:50 -0300

    Added access token to subastify client

--- src/clients/subastify.ts
+++ src/clients/subastify.ts
@@ -1,14 +1,14 @@
-import { AxiosInstance } from 'axios';
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
 
-import IAuction from 'src/model/IAuction';
-import IAuctionOptions from 'src/model/IAuctionOptions';
+import IAuction, { INewAuction } from 'src/model/IAuction';
 
 
 export interface ISubastifyClient {
-    createAuction(auctionOptions: IAuctionOptions): Promise<IAuction>;
+    createAuction(auctionOptions: INewAuction): Promise<IAuction>;
     getRecentAuctions(): Promise<IAuction[]>;
     getAuctions(): Promise<IAuction[]>;
     getAuction(id: number): Promise<IAuction>;
+    setAccessToken(token: string | undefined): void
 }
 
 const AUCTIONS_ENDPOINT = '/auctions'
@@ -16,21 +16,37 @@ const RECENT_AUCTIONS_ENDPOINT = '/auctions/recent'
 
 export class SubastifyClient implements ISubastifyClient {
 
+    private accessToken: string;
+
     constructor(private http: AxiosInstance) { }
 
-    public createAuction(auctionOptions: IAuctionOptions): Promise<IAuction> {
-        return this.http.post(AUCTIONS_ENDPOINT, auctionOptions).then(response => response.data);
+    public createAuction(auctionOptions: INewAuction): Promise<IAuction> {
+        return this.http.post(AUCTIONS_ENDPOINT, auctionOptions, this.options).then(response => response.data);
     }
 
     public getAuctions() {
-        return this.http.get(AUCTIONS_ENDPOINT).then(response => response.data);
+        return this.http.get(AUCTIONS_ENDPOINT, this.options).then(response => response.data);
     }
 
     public getRecentAuctions() {
-        return this.http.get(RECENT_AUCTIONS_ENDPOINT).then(response => response.data);
+        return this.http.get(RECENT_AUCTIONS_ENDPOINT, this.options).then(response => response.data);
     }
 
     public getAuction(id: number) {
-      return this.http.get(`${AUCTIONS_ENDPOINT}/${id}`).then(response => response.data);
+        return this.http.get(`${AUCTIONS_ENDPOINT}/${id}`, this.options).then(response => response.data);
+    }
+
+    public setAccessToken(token: string){
+        this.accessToken = token;
+    }
+
+    private get options(): AxiosRequestConfig {
+        const options = {headers:{}}
+        
+        if(this.accessToken){
+            options.headers = `Bearer ${this.accessToken}` 
+        }
+
+        return options;
     }
 }
